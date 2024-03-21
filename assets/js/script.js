fa18fa3bf6f8c05f4f16f187ff258c5c364b19b8
Miteshvaghela <vaghela_miteshk@yahoo.com>
2024-03-21 12:34:37 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
 /***
- * 
- * var URL = window.URL || window.webkitURL;
-var video = document.getElementsByTagName('video')[0];
-function onChange() {
-    var fileItem = document.getElementById('fileItem');
-    var files = fileItem.files;
-    var file = files[0];
-    var url = URL.createObjectURL(file);
-    console.log(file);
-    console.log(url);
+ * Author : Mitesh vaghela - vaghela_mieshk@yahoo.com
+ * Description : Upload a video and convert it to blob format and play it in video using javascript.
+ */
+
+
+
+let URL = window.location.URL || window.webkitURL;  
+
+const videoUpload = () => {    
+    let video = document.getElementById('video');
+    let fileItem = document.getElementById('file');
+    let files = fileItem.files;
+    let file = files[0];
+    let url = URL.createObjectURL(file);
     video.src = url;
-    video.load();
-    video.onloadeddata = function() {
-        video.play();
-    }
 }
 
- */
+
+const submitForm = (e) => {
+    e.preventDefault();
+    let video = document.getElementById('video'); 
+    video.play();  
+    return false;
+
+}
+
+
+
+
